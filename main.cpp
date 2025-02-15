781f60bbe2597b35053b8439b323ff7d8521c9fe
DieguinCode <diegocaracasmendes02@gmail.com>
2025-02-15 14:03:06 -0300

    Version 1.0 Finished

--- main.cpp
+++ main.cpp
@@ -54,36 +54,118 @@ vec3 randomDirection(){
     return vec3{std::cos(angle), std::sin(angle), 0.0};
 }
 
-// É chamada a cada frame para verificar inteseção da particula com algum segmento
-void checkIntersect(){
-    // TODO
+// Lida com o caso especial que o ponto Q é colinear ao segmento PR e verifica se
+// Q está dentro dos limites do segmento da reta
+bool onSegment(const ponto2D& p, const ponto2D& q, const ponto2D& r) {
+    return q.x <= std::max(p.x, r.x) && q.x >= std::min(p.x, r.x) &&
+           q.y <= std::max(p.y, r.y) && q.y >= std::min(p.y, r.y);
 }
 
-//Calcula a Normal de um Segmento de Reta --> TODO : Check se está correto
-vec3 calculateNormal(const ponto2D& p1, const ponto2D& p2){
-    
-    // Vetor Direção do Segmento
-    double dx = p2.x - p1.x;
-    double dy = p2.y - p1.y;
+// Usa Cross Product para verificar a orientação entre o ponto e o Segmento
+// 0 --> Colinear
+// 1 --> Sentido Horário
+// 2 --> Sentido Anti-Horário
 
-    // Em 2D: Normal 90Graus para esquerda ou 90Graus para a direita
-    vec3 n{-dy, dx, 0.0};
+// Mesma ideia usada em Triangulação de pontos.
+int orientation(const ponto2D& p, const ponto2D& q, const ponto2D& r) {
+    double val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+    if (val == 0) return 0;
+    return (val > 0) ? 1 : 2;
+}
 
-    // Vetor Origem do plano cartesiano --> Ponto Medio do Segmento
-    double midpointX = (p1.x + p2.x) / 2.0;
-    double midpointY = (p1.y + p2.y) / 2.0;
+// A ideia vai ser lidar com: a particula no frame atual, a particula do frame seguinte (projetada dado a dir),
+// o ponto a e o ponto b (Segmento de reta que pode gerar a colisão).
+bool doIntersect(const ponto2D& p1, const ponto2D& q1, const ponto2D& p2, const ponto2D& q2) {
+    int o1 = orientation(p1, q1, p2);
+    int o2 = orientation(p1, q1, q2);
+    int o3 = orientation(p2, q2, p1);
+    int o4 = orientation(p2, q2, q1);
 
-    // Vetor Ponto Medio --> Origem do plano cartesiano
-    double originX = -midpointX;
-    double originY = -midpointY;
+    if (o1 != o2 && o3 != o4) {return true;} // Caso Base: Retas que se cruzam com sentidos e angulos distintos.
 
-    if(n.dot(vec3{originX, originY, 0.0}) < 0){
-        vec3 newN{-n.get_x(), -n.get_y(), 0.0};
-        newN.normalize();
-        return newN; 
-    }else{
-        n.normalize();
-        return n;
+    //Exemplo:
+    /*
+
+                                a
+                                |
+                                |
+                                |    
+        ponto_neste_frame ------------- ponto_no_proximo_frame
+                                |
+                                |
+                                |
+                                b
+    */
+
+
+    // Caso Especial de Colinearidade
+    if (o1 == 0 && onSegment(p1, p2, q1)) {return true;}
+    if (o2 == 0 && onSegment(p1, q2, q1)) {return true;}
+    if (o3 == 0 && onSegment(p2, p1, q2)) {return true;}
+    if (o4 == 0 && onSegment(p2, q1, q2)) {return true;}
+
+    return false;
+}
+
+//Calcula a Normal de um Segmento de Reta
+vec3 calculateNormal(const ponto2D& a, const ponto2D& b){
+    vec3 normal{b.y - a.y, a.x - b.x, 0.0};
+    normal.normalize();
+    return normal;
+}
+
+// Reflete uma direção usando a normal do segmento
+vec3 reflect(const vec3& dir, const vec3& normal) {
+    double dotProduct = dir.dot(normal);
+    vec3 reflection{dir.get_x() - 2 * dotProduct * normal.get_x(), dir.get_y() - 2 * dotProduct * normal.get_y(), 0.0};
+    return reflection;
+}
+
+bool pointIntersectsSegment(const ponto2D& point, const vec3& dir, const ponto2D& a, const ponto2D& b) {
+    ponto2D projectedPoint;
+    projectedPoint.x = point.x + dir.get_x() * speed;
+    projectedPoint.y = point.y + dir.get_y() * speed;
+    return doIntersect(point, projectedPoint, a, b);
+}
+
+
+// É chamada a cada frame para verificar inteseção da particula com algum segmento
+void checkIntersect(const ponto2D& a, const ponto2D& b){
+    for (auto& particle : particles) {
+        ponto2D& point = particle.first;
+        vec3& dir = particle.second;
+        
+        if (pointIntersectsSegment(point, dir, a, b)) {
+            std::cout << "Colisão detectada!!!" << std::endl;
+
+            vec3 normal = calculateNormal(a, b);
+            vec3 newDirection = reflect(dir, normal);
+
+            dir = newDirection;
+        }
+    }
+}
+
+// Normal dos Limites da janela (Trivial)
+vec3 getBorderNormal(const ponto2D& pos) {
+    if (pos.x <= xMin) return vec3{1.0f, 0.0f, 0.0f};
+    if (pos.x >= xMax) return vec3{-1.0f, 0.0f, 0.0f};
+    if (pos.y <= yMin) return vec3{0.0f, 1.0f, 0.0f};
+    if (pos.y >= yMax) return vec3{0.0f, -1.0f, 0.0f};
+    return vec3{0.0f, 0.0f, 0.0f};
+}
+
+// Check a colisão com os limites da janela gráfica
+void intersectWithLimits() {
+    for (auto& particle : particles) {
+        ponto2D& pos = particle.first;
+        vec3& dir = particle.second;
+
+        vec3 normal = getBorderNormal(pos);
+        if (normal.get_x() != 0.0f || normal.get_y() != 0.0f) {
+            vec3 newDirection = reflect(dir, normal);
+            dir = newDirection;
+        }
     }
 }
 
@@ -246,7 +328,7 @@ int main(){
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Segment Intersection", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Particle Physics", nullptr, nullptr);
     if (!window) {
         std::cerr << "Erro ao criar janela GLFW." << std::endl;
         glfwTerminate();
@@ -313,6 +395,7 @@ int main(){
         glDrawArrays(GL_LINES, 0, 4);
 
         moveParticles();
+        intersectWithLimits();
         for(const auto& p : particles){
             drawPoint(p.first, shaderProgram, projection, 0.5f, 0.5f, 0.5f);
         }
@@ -323,10 +406,12 @@ int main(){
             if(segs.size() % 2 == 0){
                 for(int i = 0; i < segs.size(); i = i + 2){
                     drawSegment(segs[i], segs[i+1], shaderProgram, projection, 1.0f, 0.0f, 0.0f);
+                    checkIntersect(segs[i], segs[i+1]);
                 }  
             }else{
                 for(int i = 0; i < segs.size() - 1; i = i + 2){
                     drawSegment(segs[i], segs[i+1], shaderProgram, projection, 1.0f, 0.0f, 0.0f);
+                    checkIntersect(segs[i], segs[i+1]);
                 }
             }
 
